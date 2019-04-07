deb18c1943dce40cfe5b435f277a621a8215a494
nomangul <nomangul2001@gmail.com>
2019-04-08 02:56:55 +0500

    modified: folders structure

--- src/store/fetchBooks.js
+++
@@ -1,9 +0,0 @@
-import Axios from "axios";
-import { API_KEY } from "../constant/API_KEY";
-
-export const fetchBooks = async () => {
-  const req = await Axios.get(
-    `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=5&filter=paid-ebooks&projection=lite&key=${API_KEY}`
-  );
-  return console.log(req);
-};
