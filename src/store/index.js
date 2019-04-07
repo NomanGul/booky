f3cc2ca89cd963ca8ebbfb036ce7d2a903fec421
nomangul <nomangul2001@gmail.com>
2019-04-08 03:07:33 +0500

    modified: main saga name to rootSaga added: redux devtools integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "remote-redux-devtools";
 import createSagaMiddleware from "redux-saga";
-
 import reducers from "./reducers";
-import sagas from "./sagas";
+import rootSaga from "./sagas";
 
 const sagasMiddleware = createSagaMiddleware();
 
-export const store = createStore(reducers, applyMiddleware(sagasMiddleware));
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagasMiddleware))
+);
 
-sagasMiddleware.run(sagas);
+sagasMiddleware.run(rootSaga);
