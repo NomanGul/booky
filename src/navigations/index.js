69065b26b83fe759d4cef833baeb17eb0d3259fd
nomangul <nomangul2001@gmail.com>
2019-04-08 03:08:43 +0500

    modified: status bar for all screens

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,7 +1,9 @@
+import React from "react";
+import { StatusBar } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Home, BooksList, BookDetails } from "../screens";
 
-const AppNavigator = createStackNavigator(
+const AppStackNavigator = createStackNavigator(
   {
     Home,
     BooksList,
@@ -15,4 +17,15 @@ const AppNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppStackNavigator);
+
+const AppNavigator = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#ffff00" barStyle="dark-content" />
+      <AppContainer />
+    </>
+  );
+};
+
+export default AppNavigator;
