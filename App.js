fadc25c7f79657403ca8436552506735bea7a4f7
nomangul <nomangul2001@gmail.com>
2019-04-08 02:58:09 +0500

    added: redux store provider removed: unusal code

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import AppNavigator from "./src/navigations";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
-export default class App extends Component {
-  render() {
-    return <AppNavigator />;
-  }
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+};
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#F5FCFF"
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: "center",
-//     margin: 10
-//   },
-//   instructions: {
-//     textAlign: "center",
-//     color: "#333333",
-//     marginBottom: 5
-//   }
-// });
+export default App;
