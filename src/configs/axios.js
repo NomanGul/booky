19c0a2e2943a9dc5974c5768417c2580b6394bce
nomangul <nomangul2001@gmail.com>
2019-04-08 03:04:31 +0500

    added: to gitignore

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -5,7 +5,7 @@ export const axios = Axios.create({
   baseURL: "https://www.googleapis.com/books/v1/",
   headers: {
     "X-Android-Package": "com.booky",
-    "X-Android-Cert": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    "X-Android-Cert": "228a451245483f2cfa8b8831b055d91e169f375b"
   },
   params: {
     key: API_KEY
