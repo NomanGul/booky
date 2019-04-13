8883159c21affda66151c37217f55be8434bffcb
nomangul <nomangul2001@gmail.com>
2019-04-14 01:04:22 +0500

    refactored: existing code! styles splitting up

---
+++ src/components/BookCard/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+import { width, height } from "../../utils/dimensions";
+
+export const styles = StyleSheet.create({
+  bookCardContainer: {
+    width: width * 0.3,
+    height: height * 0.4,
+    aspectRatio: 0.75,
+    padding: width * 0.02,
+    marginBottom: height * 0.02
+  },
+  imgContainer: {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    backgroundColor: "#ff0"
+  },
+  img: {
+    resizeMode: "stretch",
+    width: "100%",
+    height: "100%"
+  },
+  noThumbnailImg: {
+    flex: 1,
+    width: undefined,
+    height: undefined
+  }
+});
