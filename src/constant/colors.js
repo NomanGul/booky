2743b169af5527e022259bd0cfa2b93607fb10db
nomangul <nomangul2001@gmail.com>
2019-05-12 22:47:00 +0500

    add some constant colors for book details screen

--- src/constant/colors.js
+++ src/constant/colors.js
@@ -1,5 +1,8 @@
 export const COLORS = {
   APP_BACKGROUND: "#ffff00",
   WHITE: "#ffffff",
-  GREY: "#454545"
+  GREY: "#454545",
+  LIGHT_GREY: "rgba(45,45,45,0.5)",
+  BLUE: "#549ae6",
+  RED: "#e05568"
 };
